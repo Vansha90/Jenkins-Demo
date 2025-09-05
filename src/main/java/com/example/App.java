b2f2f76b2f8243831f31d3af4a65c0559bcbbc96
Ubuntu <ubuntu@ip-172-31-114-38.ec2.internal>
2025-09-05 13:47:16 +0000

    webhook change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!:testing webhooks" );
+        System.out.println( "Hello World!:testing webhooks !!" );
     }
 }
